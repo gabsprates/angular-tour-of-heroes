93677b1acf11d791577f13cf20ec22df10a0df53
Gabriel Prates <gabsprates@gmail.com>
2017-09-08 17:54:04 -0300

    get-hero-by-id

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import { Headers, Http } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) { }
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(res => res.json().data as Hero[])
+      .catch(this.handleError);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('shits happens', error);
+    return Promise.reject(error.message || error);
   }
 
   getHero(id: Number): Promise<Hero> {
